22a10ace5c9b91fa56c51776ca16fe6b9a49f14f
Jan Schützke <jan.schuetzke@kit.edu>
2022-04-12 16:02:53 -0700

    streamlined spectra creation; added test data grid (9) instead of 3 samples

--- spectra_from_config.py
+++ spectra_from_config.py
@@ -12,16 +12,37 @@ shift_range = 50 # for now, we shift all peaks independently
 variation_range = 0.05 # +/- of absolute height for each peak
 kernel_range = (2, 5) # min and max for guassian kernel sizes
 
-n_test = 3 # predefined variations, just here for clarity
+n_test = 9 # predefined variations, just here for clarity
 n_val = 10
 n_train = 50
+
+rng = np.random.default_rng(2022)
+
+def vary_peaks(position_list, height_list):
+    # since we set a boundary parameter, positions should never exceed range
+    # still applying clip to be sure
+    new_positions = np.clip(np.array([
+        rng.integers(f-shift_range, f+shift_range) for f in position_list
+        ]), 0, 4999)
+    new_heights = np.clip(np.array([
+        rng.uniform(f-variation_range, f+variation_range) for f in height_list
+        ]), 0, 2)
+    return new_positions, new_heights
+
+def add_noise(input_scan):
+    scan = input_scan.copy() #avoiding pointer confusion
+    gaus = 1/3 * np.clip(rng.normal(0, 1, scan.size), -3, 3)
+    gaus = (gaus*.5)+.5
+    noise_lvl = rng.uniform(.01, .02)
+    scan += gaus * (noise_lvl*np.max(scan))
+    return scan  
 
 def main():   
     with open('dataset_configs/dataset500.json', 'r') as file:
         config = json.load(file)
     datapoints = config['datapoints']
     n_classes = config['classes']
-
+    # initialize arrays to fill
     x_train = np.zeros([(n_train*n_classes), datapoints])
     x_val = np.zeros([(n_val*n_classes), datapoints])
     x_test = np.zeros([(n_test*n_classes), datapoints])
@@ -29,45 +50,58 @@ def main():
     y_val = np.zeros(n_val*n_classes)
     y_test = np.zeros(n_test*n_classes)
 
-    rng = np.random.default_rng(2022)
-
     spectra = config['spectra']
     for i, phase in enumerate(tqdm(spectra.keys())):
-        peaks = np.array(spectra[phase]['peak_positions'])
+        peak_positions = np.array(spectra[phase]['peak_positions'])
         peak_heights = np.array(spectra[phase]['peak_heights'])
-        # test - use exact values + defined variations
-        scan = np.zeros([3, datapoints])
-        scan[0,peaks] = peak_heights
-        scan[1,np.clip(np.array(peaks)-25, 0, 4999)] = peak_heights
-        scan[2,np.clip(np.array(peaks)+25, 0, 4999)] = peak_heights
-        x_test[i*n_test] = gaussian_filter1d(scan[0], 2, mode='constant')
-        x_test[i*n_test+1] = gaussian_filter1d(scan[1], 2, mode='constant')
-        x_test[i*n_test+2] = gaussian_filter1d(scan[2], 2, mode='constant')
-        y_test[i*n_test:(i+1)*n_test] = i
-        for j in range(n_val):
-            # apply shift and clip peak positions outside range
-            new_peaks = np.clip(np.array([rng.integers(f-shift_range, f+shift_range) for f in peaks]), 0, 4999)
-            # apply height variation and clip peaks smaller 0
-            new_heights = np.clip(np.array([rng.uniform(f-variation_range, f+variation_range) for f in peak_heights]), 0, 2)
-            scan = np.zeros(datapoints)
-            scan[new_peaks] = new_heights
-            scan = gaussian_filter1d(scan, rng.uniform(*kernel_range), mode='constant')
-            x_val[(i*n_val)+j] = scan
-        y_val[i*n_val:(i+1)*n_val] = i
+        
+        # train data
         for j in range(n_train):
-            # apply shift and clip peak positions outside range
-            new_peaks = np.clip(np.array([rng.integers(f-shift_range, f+shift_range) for f in peaks]), 0, 4999)
-            # apply height variation and clip peaks smaller 0
-            new_heights = np.clip(np.array([rng.uniform(f-variation_range, f+variation_range) for f in peak_heights]), 0, 2)
             scan = np.zeros(datapoints)
-            scan[new_peaks] = new_heights
-            scan = gaussian_filter1d(scan, rng.uniform(*kernel_range), mode='constant')
-            gaus = 1/3 * np.clip(rng.normal(0, 1, scan.size), -3, 3)
-            gaus = (gaus*.5)+.5
-            noise_lvl = rng.uniform(.01, .02)
-            scan += gaus * (noise_lvl*np.max(scan))
+            # apply shifts
+            new_pos, new_hi = vary_peaks(peak_positions, peak_heights)
+            scan[new_pos] = new_hi
+            # convolve with gaussian kernel
+            scan = gaussian_filter1d(scan, rng.uniform(*kernel_range), 
+                                     mode='constant')
+            # adding gaussian noise to training data
+            scan = add_noise(scan)
             x_train[(i*n_train)+j] = scan
         y_train[i*n_train:(i+1)*n_train] = i
+        
+        # validation data
+        for j in range(n_val):
+            scan = np.zeros(datapoints)
+            # apply shifts
+            new_pos, new_hi = vary_peaks(peak_positions, peak_heights)
+            scan[new_pos] = new_hi
+            # convolve with gaussian kernel
+            scan = gaussian_filter1d(scan, rng.uniform(*kernel_range), 
+                                     mode='constant')
+            x_val[(i*n_val)+j] = scan
+        y_val[i*n_val:(i+1)*n_val] = i
+        
+        # test - use exact values + defined variations
+        scan = np.zeros([n_test, datapoints])
+        scan[0,peak_positions] = peak_heights # exact reference
+        # two variants with exact heights and -25/+25 position shifts
+        pos_left = np.clip(np.array(peak_positions)-25, 0, 4999)
+        pos_right = np.clip(np.array(peak_positions)+25, 0, 4999)
+        scan[1,pos_left] = peak_heights
+        scan[2,pos_right] = peak_heights
+        # three variants with -25/0/+25 shifts and 0.2 higher intensities
+        new_heights = [f+.2 if f != 1. else f for f in peak_heights]
+        scan[3,peak_positions] = new_heights
+        scan[4,pos_left] = new_heights
+        scan[5,pos_right] = new_heights
+        # three variants with -25/0/+25 shifts and 0.2 lower intensities
+        new_heights = [f-.2 if f != 1. else f for f in peak_heights]
+        scan[6,peak_positions] = new_heights
+        scan[7,pos_left] = new_heights
+        scan[8,pos_right] = new_heights
+        for j in range(n_test):
+            x_test[i*n_test+j] = gaussian_filter1d(scan[j], 2, mode='constant')
+        y_test[i*n_test:(i+1)*n_test] = i   
 
     np.save('x_train.npy', x_train)
     np.save('y_train.npy', y_train)

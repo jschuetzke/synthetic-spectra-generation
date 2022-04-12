f42ded17745d18121713c15dfc31dc7c8f386776
Jan Schützke <jan.schuetzke@kit.edu>
2022-04-12 15:24:09 -0700

    changed dataset config structure

--- dataset_config_generator.py
+++ dataset_config_generator.py
@@ -8,24 +8,32 @@ import numpy as np
 
 # PARAMETERS
 n_datapoints = 5000
-n_phases = 50
+n_classes = 500
 min_peaks = 2
 max_peaks = 10
-max_height = 1000
+max_height = 100
 
 def main():
     rng = np.random.default_rng(2022)
-    config = {}
-    for phase in range(n_phases):
+    config = {
+        'datapoints' : n_datapoints,
+        'classes' : n_classes,
+        'min_peaks' : min_peaks,
+        'max_peaks' : max_peaks,
+        'max_height' : max_height
+        }
+    spectra = {}
+    for phase in range(n_classes):
         n_peaks = rng.integers(min_peaks, max_peaks, endpoint=True)
         peak_positions = rng.integers(0, n_datapoints, n_peaks, endpoint=True)
         peak_heights = rng.integers(0, max_height, n_peaks)
         peak_heights = np.round(peak_heights / np.max(peak_heights), 3)
         phase_dict = {'peak_positions': np.sort(peak_positions).tolist(),
                       'peak_heights' : peak_heights[np.argsort(peak_positions)].tolist()}
-        config[phase] = phase_dict
-
-    with open('dataset_configs/dataset1.json', 'w') as file:
+        spectra[phase] = phase_dict
+    
+    config['spectra'] = spectra
+    with open('dataset_configs/dataset500.json', 'w') as file:
         json.dump(config, file)
 
 if __name__ == '__main__':
